b9914c9accb02bc8793ca0cf6b3553a5fda285f1
EmilianoCBE <e.carrizales1305@gmail.com>
2026-01-19 15:54:02 -0600

    Cambio a php

---
+++ includes/head.php
@@ -0,0 +1,35 @@
+<head>
+    <title><?php echo isset($page_title) ? $page_title : 'IMA Express'; ?></title>
+    
+    <link
+      rel="icon"
+      href="images/IMA_logo_white.png"
+      type="image/gif"
+      sizes="16x16"
+    />
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta
+      content="IMA Express - Transportation, Cargo & Logistics HTML Template"
+      name="description"
+    />
+    <meta content="" name="keywords" />
+    <meta content="" name="author" />
+    <link
+      href="css/bootstrap.min.css"
+      rel="stylesheet"
+      type="text/css"
+      id="bootstrap"
+    />
+    <link href="css/plugins.css" rel="stylesheet" type="text/css" />
+    <link href="css/swiper.css" rel="stylesheet" type="text/css" />
+    <link href="css/swiper-custom-1.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/coloring.css" rel="stylesheet" type="text/css" />
+    <link
+      id="colors"
+      href="css/colors/scheme-01.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+</head>
